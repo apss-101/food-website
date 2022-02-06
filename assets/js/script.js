d464f98f322c2b380226c8fb4f9362ced7209cc6
NazAbdul <95302790+NazAbdul@users.noreply.github.com>
2022-02-06 22:36:06 +0000

    Added a button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
-//navbar hamburger function
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+THEMEALDB_BASE_URL = "www.themealdb.com/api/json/v1/1/search.php?";
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+//navbar hamburger function
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const searchForm = document.getElementById("search-form");
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
 });
 
+function onSearchSubmit(event) {
+  event.preventDefault();
+  const searchInput = document.getElementById("search-input");
+  const url =
+    "www.themealdb.com/api/json/v1/1/search.php?s=" + searchInput.value;
+  console.log(url);
+  fetch(url, {
+    method: "GET",
+  })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error.message);
+    });
+}
+searchForm.onsubmit = onSearchSubmit;
