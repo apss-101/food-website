6f0a8e1d38a7d2824330511234bb6c247395cc22
Humayra Hayern <hayern786@hotmail.com>
2022-02-12 00:03:23 +0000

    api success

--- assets/js/recipescript.js
+++ assets/js/recipescript.js
@@ -1,17 +1,28 @@
-let nextPageToken = ""
-function getVideos(){
-  fetch ("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&q=food%20recipes&key=AIzaSyCbtz8mGvgrzhpn_bGu6m5-dHO5vBzHGlE&pageToken=+nextPageToken")
-  .then((result)=>{
-    return result.json()
-  }) 
-  .then((data)=>{
-    console.log(data)
-    let videos = data.items
-    nextPageToken = data.nextPageToken
-    let videoContainer = document.querySelector(".youtube_container")
-    for(video of videos){
-      videoContainer.innerHTML += '<img src="${video.snippet.thumbnails}.high.url}">' 
-
-    }
+fetch(
+  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&q=recipes&key=AIzaSyAhbUq2j3C1NR5yXlBQ2DaajGY0AhY252I"
+)
+  .then((result) => {
+    return result.json();
   })
-}
+  .then((data) => {
+    console.log(data);
+    let videos = data.items;
+    console.log(videos);
+    let videoContainer = document.getElementById("youtubeContainer");
+    for (video of videos) {
+      var div = document.createElement("div");
+      div.classList.add("video-card");
+      function videoTitle(title) {
+        var trimmedTitle = title.slice(0, 15);
+        return (trimmedTitle = `${trimmedTitle}(...)`);
+      }
+      div.innerHTML =
+        "<h3>" +
+        videoTitle(video.snippet.title) +
+        "</h3>" +
+        '<iframe width="560" height="315" src="https://www.youtube.com/embed/' +
+        video.id.videoId +
+        '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+      videoContainer.appendChild(div);
+    }
+  });
