fcfd8aba5d4295870e1ec78e8f0f5b822a51b499
apss_101 <ajaysaul13@hotmail.com>
2022-02-12 10:06:01 +0000

    form location

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,17 @@ THEMEALDB_BASE_URL = "https://www.themealdb.com/api/json/v1/1/search.php?";
 //navbar hamburger function
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-const searchForm = document.getElementById("search-form");
+
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
 
+const searchForm = document.getElementById("search-form");
+
 function onSearchSubmit(event) {
   event.preventDefault();
-  const searchInput = document.getElementById("search-input").value;
+  const searchInput = document.getElementById("search").value;
   const url = THEMEALDB_BASE_URL + "s=" + searchInput;
   console.log(url);
   fetch(url)
@@ -26,3 +28,29 @@ function onSearchSubmit(event) {
     });
 }
 searchForm.onsubmit = onSearchSubmit;
+
+/* new javascript*/
+/*
+function generateHTML(results) {
+  let generatedHTML = "";
+  results.map((result) => {
+    generatedHTML += `
+     <div class="item">
+        <img src=`${result.recipe.image}` alt="img">
+        <div class="flex-container">
+          <h1 class="title">`${result.recipe.label}`</h1>
+          <a class="view-btn" target="_blank" href=`${
+            result.recipe.url
+          }`>View Recipe</a>
+        </div>
+        <p class="item-data">Calories: `${result.recipe.calories.toFixed(2)}`</p>
+        <p class="item-data">Diet label: `${
+          result.recipe.dietLabels.length > 0
+            ? result.recipe.dietLabels
+            : "No Data Found"
+        }`</p>
+        <p class="item-data">Health labels: `${result.recipe.healthLabels}`</p>
+      </div>;`
+  });
+  searchResultDiv.innerHTML = generatedHTML;
+}
